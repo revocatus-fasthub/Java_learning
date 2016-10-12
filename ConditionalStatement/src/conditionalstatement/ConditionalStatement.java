2d402e60df3084d310e485f4582cddc81a8b951d
revocatus-fasthub <revocatus.robert@fasthub.co.tz>
2016-10-12 16:59:09 +0300

    java learning files

---
+++ ConditionalStatement/src/conditionalstatement/ConditionalStatement.java
@@ -0,0 +1,22 @@
+package conditionalstatement;
+
+import java.util.*;
+
+public class ConditionalStatement {
+
+    public static void main(String[] args) {
+      Scanner reader = new Scanner(System.in);
+      System.out.println("Input Now: ");
+      int input = reader.nextInt();
+      
+      if (input == 10)
+        {
+      System.out.println("YES!");
+        }
+      else
+        {
+      System.out.println("NO!");
+        }
+    }
+    
+}
